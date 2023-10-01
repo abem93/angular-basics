d110cdc8edb39a9e1bdee280a27e312a2e52b762
Abraham Mejia <aimejia4@gmail.com>
2023-10-01 17:45:53 -0500

    Angular basics Completed

---
+++ src/app/success-alert/success.component.ts
@@ -0,0 +1,15 @@
+import { Component } from "@angular/core";
+
+@Component ({
+  selector: 'success-alert',
+  templateUrl: './success.component.html',
+  styles: [`
+    p{
+      color: green;
+      font-size: 2em;
+    }
+  `]
+})
+export class SuccessComponent {
+
+}
